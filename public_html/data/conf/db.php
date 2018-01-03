b3bda7594f8fb3102394b7d0ad1827ff2ef1a53a
tuyao <450175465@qq.com>
2018-01-03 15:45:06 +0800

    a

---
+++ public_html/data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'oulanzhi',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'tuyao19910715',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'tb_',
+    //密钥
+    "AUTHCODE" => 'iff0UpX3F4s6Q3VPPj',
+    //cookies
+    "COOKIE_PREFIX" => '2z5hLY_',
+);
