d1c9964f9886538139964818b0eb052ddc6b7d02
Admin <zhouqian11989@sina.com>
2018-01-07 17:30:20 +0800

    test

--- public_html/application/User/Controller/CenterController.class.php
+++ public_html/application/User/Controller/CenterController.class.php
@@ -19,120 +19,19 @@ class CenterController extends MemberbaseController
         $this->display(':center');
     }
 
-
-    //用户关注的画板
-    public function draw()
-    {
-        $users = $this->user;
-        $uid = $users['id'];
-        //用户关注的画板
-        $hbgz_model = M("hbgz");
-        $hbgz = $hbgz_model
-            ->alias("a")
-            ->join("tb_huaban ON a.hbgz_hbid = tb_huaban.hb_id")
-            ->where(array("hbgz_uid" =>$uid))
-            ->select();
-        $this->assign($users);
-        $this->assign($hbgz);
-        $this->display();
-        //var_dump($love);
-    }
-
-    //用户关注的兴趣点
-    public function interest()
-    {
-        $users = $this->user;
-        $uid = $users['id'];
-        $xqUsers_model = M('Xqd_guanzhu');
-        $xqgz = $xqUsers_model
-            ->alias("a")
-            ->join("tb_xingqu ON a.xqdgz_xqid = tb_xingqu.xq_id")
-            ->where(array("xqdgz_uid" => $uid))
-            ->select();
-        $this->assign($users);
-        $this->assign($xqgz);
-        $this->display();
-        //var_dump($love);
-    }
-
-    //用户关注的用户
-    public function user()
-    {
-        $users = $this->user;
-        $uid = $users['id'];
-        $usergz_model = M('Yonghu_gz');
-        $usergz = $usergz_model
-            ->alias("a")
-            ->join("tb_users ON a.usergz_uid_childid = tb_users.id")
-            ->where(array("usergz_uid_pid" => $uid))
-            ->select();
-        $this->assign($users);
-        $this->assign($usergz);
-        $this->display();
-        //var_dump($love);
-    }
-
-
-    //用户自己的采集
-    public function collect()
-    {
-        $users = $this->user;
-        $uid = $users['id'];
-        $caiji_model = M("Caiji");
-        $caiji = $caiji_model
-            ->alias("a")
-            ->join("tb_posts ON a.cj_post_id = tb_posts.id")
-            ->where(array("cj_u_id" => $uid))
-            ->select();
-        $this->assign($users);
-        $this->assign($caiji);
-        //var_dump($love);
-    }
-
-    //用户自己的画板
-    public function drawSelf()
-    {
-        $users = $this->user;
-        $uid = $users['id'];
-        $hbuser_model = M("Hb_users");
-        $hbuser = $hbuser_model
-            ->alias("a")
-            ->join("tb_huaban ON a.hbusers_hb_id = tb_huaban.hb_id")
-            ->where(array("hbusers_users_id" => $uid))
-            ->select();
-        $this->assign($users);
-        $this->assign($hbuser);
-        //var_dump($love);
-    }
-
-    //用户自己的喜欢
-    public function love()
-    {
-        $users = $this->user;
-        $uid = $users['id'];
-        $love_model = M("Love");
-        $love = $love_model
-            ->alias("a")
-            ->join("tb_posts ON a.love_posts_id = tb_posts.id")
-            ->where(array("love_users_id" => $uid))
-            ->select();
-        $this->assign($users);
-        $this->assign($love);
-        //var_dump($love);
-    }
-
-    //用户自己的标签
-    public function label()
-    {
-        $users = $this->user;
-        $uid = $users['id'];
-        $tag_model = M("Tag");
-        $tag = $tag_model
-            ->where(array("tag_user_id" => $uid))
-            ->select();
-        $this->assign($users);
-        $this->assign($tag);
-        //var_dump($tag);
+    public function userCount(){
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- public_html/application/User/Controller/CollectController.class.php
+++ public_html/application/User/Controller/CollectController.class.php
@@ -13,19 +13,21 @@ use Common\Controller\MemberbaseController;
 
 class CollectController extends MemberbaseController
 {
-   //用户中心采集列表显示
-    public function index(){
-       $uid = sp_get_current_userid();
-       $caiji_model = M("Caiji");
-       $where = array("cj_u_id"=>$uid);
-       $caiji = $caiji_model->where($where)
-           ->order("cj_time")->alias("a")->join("tb_posts on a.cj_posts_id = tb_posts.id")->select();
-        $this->assign("caiji",$caiji);
+    //用户中心采集列表显示
+    public function index()
+    {
+        $uid = sp_get_current_userid();
+        $caiji_model = M("Caiji");
+        $where = array("cj_u_id" => $uid);
+        $caiji = $caiji_model->where($where)
+            ->order("cj_time")->alias("a")->join("tb_posts on a.cj_posts_id = tb_posts.id")->select();
+        $this->assign("caiji", $caiji);
     }
 
     //用户采集
-    public function do_collect(){
-       $id = I("id");
+    public function do_collect()
+    {
+        $id = I("id");
         var_dump($id);
     }
 
@@ -42,7 +44,32 @@ class CollectController extends MemberbaseController
     }
 
     //用户采集删除
-    public function delete_collect(){
+    public function delete_collect()
+    {
 
+    }
+
+    //用户自己的标签
+    public function label()
+    {
+        $uid = sp_get_current_userid();
+        $tag_model = M("Tag");
+        $tag = $tag_model
+            ->where(array("tag_user_id" => $uid))
+            ->select();
+        echo json_encode($tag);
+    }
+
+    //用户自己的喜欢
+    public function love()
+    {
+        $uid = sp_get_current_userid();
+        $love_model = M("Love");
+        $love = $love_model
+            ->alias("a")
+            ->join("tb_posts ON a.love_posts_id = tb_posts.id")
+            ->where(array("love_users_id" => $uid))
+            ->select();
+        echo json_encode($love);
     }
 }
